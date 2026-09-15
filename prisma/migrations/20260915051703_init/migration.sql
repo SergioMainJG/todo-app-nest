63128c47b8a7180c3fa709f35e01b00fbf2989e0
SergioMainJG <arce.roldan.sergio@gmail.com>
2026-09-15 00:46:13 -0600

    feat: prisma settled

---
+++ prisma/migrations/20260915051703_init/migration.sql
@@ -0,0 +1,15 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "fullName" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password_hash" TEXT NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_fullName_key" ON "User"("fullName");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
